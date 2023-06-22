095edf4b34aee50d4b9a1a4c56f1670a449af24f
Murad <135635818+murkerimeow@users.noreply.github.com>
2023-06-23 00:24:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-tikToeField = [new Array(3), new Array(3), new Array(3)];
+let tikToeField = [new Array(3), new Array(3), new Array(3)];
 
 htmlFieldString = '';
 
-for (let i = 0; i < 3; i++) {
-    htmlFieldString = htmlFieldString + '<div class="row">';
-    for (let j = 0; j < 3; j++) {
-        htmlFieldString = htmlFieldString + `<div class="cell cell-${i}-${j}" data-i="${i}" data-j="${j}"></div>`;
-    }
-    htmlFieldString = htmlFieldString + '</div>';
-}
+tikToeField.forEach((i) => {
+    htmlFieldString += '<div class="row">';
+    tikToeField.forEach((j) => {
+        htmlFieldString += `<div class="cell cell-${i}-${j}" data-i="${i}" data-j="${j}"></div>`;
+    })
+    htmlFieldString += '</div>';
+});
+
+// for (let i = 0; i < tikToeField.length; i++) {
+//     htmlFieldString += '<div class="row">';
+//     for (let j = 0; j < tikToeField.length; j++) {
+//         htmlFieldString += `<div class="cell cell-${i}-${j}" data-i="${i}" data-j="${j}"></div>`;
+//     }
+//     htmlFieldString = htmlFieldString + '</div>';
+// }
 
 document.querySelector('.field').innerHTML = htmlFieldString;
 
@@ -19,17 +27,27 @@ function onFieldClick(event) {
     let i = targetElemet.dataset.i;
     let j = targetElemet.dataset.j;
 
-    if (step % 2 === 0){
-        document.querySelector(`.cell-${i}-${j}`).innerHTML = 'X';
-    } else {
-        document.querySelector(`.cell-${i}-${j}`).innerHTML = 'O';
-    }
+    (step % 2 === 0) ? document.querySelector(`.cell-${i}-${j}`).innerHTML = 'X' : document.querySelector(`.cell-${i}-${j}`).innerHTML = 'O';
+    
     step++;
 
-    isWinner();
+    // let isFilledCell;
+    // if (isFilledCell){
+    //     return;
+    // };
+   
+    checkWinner();
 }
 
-function isWinner(){
+function winnerX(){
+    alert('Победил "X"')
+};
+
+function winnerO(){
+    alert('Победил "O"')
+};
+
+function checkWinner(){
     let cell00 = document.querySelector('.cell-0-0');
     let cell01 = document.querySelector('.cell-0-1');
     let cell02 = document.querySelector('.cell-0-2');
@@ -41,39 +59,40 @@ function isWinner(){
     let cell22 = document.querySelector('.cell-2-2');
 
     if(cell00.innerHTML==='X' && cell01.innerHTML==='X' && cell02.innerHTML==='X'){
-        alert('Победил игрок №1')
+        setTimeout(winnerX, 0)
     } else if(cell10.innerHTML==='X' && cell11.innerHTML==='X' && cell12.innerHTML==='X'){
-        alert('Победил игрок №1')
+        setTimeout(winnerX, 0)
     } else if(cell20.innerHTML==='X' && cell21.innerHTML==='X' && cell22.innerHTML==='X'){
-        alert('Победил игрок №1')    
+        setTimeout(winnerX, 0)    
     } else if(cell00.innerHTML==='X' && cell11.innerHTML==='X' && cell22.innerHTML==='X'){
-        alert('Победил игрок №1')    
+        setTimeout(winnerX, 0)  
     } else if(cell02.innerHTML==='X' && cell11.innerHTML==='X' && cell20.innerHTML==='X'){
-        alert('Победил игрок №1')    
+        setTimeout(winnerX, 0)   
+    } else if(cell00.innerHTML==='X' && cell10.innerHTML==='X' && cell20.innerHTML==='X'){
+        setTimeout(winnerX, 0)
+    } else if(cell01.innerHTML==='X' && cell11.innerHTML==='X' && cell21.innerHTML==='X'){
+        setTimeout(winnerX, 0) 
+    } else if(cell02.innerHTML==='X' && cell12.innerHTML==='X' && cell22.innerHTML==='X'){
+        setTimeout(winnerX, 0) 
     } else if(cell00.innerHTML==='O' && cell01.innerHTML==='O' && cell02.innerHTML==='O'){
-        alert('Победил игрок №2')
+        setTimeout(winnerO, 0) 
     } else if(cell10.innerHTML==='O' && cell11.innerHTML==='O' && cell12.innerHTML==='O'){
-        alert('Победил игрок №2')
+        setTimeout(winnerO, 0) 
     } else if(cell20.innerHTML==='O' && cell21.innerHTML==='O' && cell22.innerHTML==='O'){
-        alert('Победил игрок №2')
+        setTimeout(winnerO, 0) 
     } else if(cell00.innerHTML==='O' && cell11.innerHTML==='O' && cell22.innerHTML==='O'){
-        alert('Победил игрок №2')    
+        setTimeout(winnerO, 0)  
     } else if(cell02.innerHTML==='O' && cell11.innerHTML==='O' && cell20.innerHTML==='O'){
-        alert('Победил игрок №2')    
-    };
+        setTimeout(winnerO, 0) 
+    } else if(cell00.innerHTML==='O' && cell10.innerHTML==='O' && cell20.innerHTML==='O'){
+        setTimeout(winnerO, 0) 
+    } else if(cell01.innerHTML==='O' && cell11.innerHTML==='O' && cell21.innerHTML==='O'){
+        setTimeout(winnerO, 0)  
+    } else if(cell02.innerHTML==='O' && cell12.innerHTML==='O' && cell22.innerHTML==='O'){
+        setTimeout(winnerO, 0) 
+    }
 }
 
+
+
 document.querySelector('.field').addEventListener('click', onFieldClick);
-
-
-//Задачи
-// function min(a, b){
-//     return a < b ? a : b;
-// }
-
-// let a = prompt('a?', );
-// let b = prompt('b?', );
-
-// let result = min(a, b);
-
-// alert(result);
